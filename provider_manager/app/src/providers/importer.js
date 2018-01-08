e7152f4741012ec3fbf74f1e099aedf6e1b414d8
Niklas <mr.n.sandstrom@gmail.com>
2018-01-08 19:58:59 +0100

    improved error handling in file import

--- provider_manager/app/src/providers/importer.js
+++ provider_manager/app/src/providers/importer.js
@@ -8,11 +8,14 @@ export default function importer(targetPath, importPath) {
 	let items = {}
 
 	glob.sync( path.resolve(targetPath, importPath) ).forEach( function( file ) {
-		let splitPath = file.split('/')
-		let dir = splitPath[splitPath.length-2]
-		let name = dir.charAt(0).toUpperCase() + dir.slice(1);
-		items[name] = require( path.resolve( file ) ).default
+		try {
+			let splitPath = file.split('/')
+			let dir = splitPath[splitPath.length-2]
+			let name = dir.charAt(0).toUpperCase() + dir.slice(1);
+			items[name] = require( path.resolve( file ) ).default
+		} catch(error) {
+			console.log(error)
+		}
 	});
-
 	return items
 }
